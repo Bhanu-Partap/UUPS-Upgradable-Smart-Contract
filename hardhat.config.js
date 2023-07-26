5110a4fe267313ec35ad8c509230b132168ee6a4
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-07-26 14:26:12 +0530

    UUPS

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,11 +8,11 @@ module.exports = {
  solidity: "0.8.10",
  networks: {
    kovan: {
-     url: `https://sepolia.infura.io/v3/ace13403824d4f809e011d9d547a4aa1${process.env.INFURA_API_KEY}`,
-     accounts: [process.env.PRIVATE_KEY],
+     url: `https://sepolia.infura.io/v3/ace13403824d4f809e011d9d547a4aa1`,
+     accounts: `56fe1fd10f0203c190640fb0bad78e9bf29d10fc2fbc97ee1ff49ee4acd2d08a`,
    },
  },
  etherscan: {
-   apiKey: process.env.ETHERSCAN_API_KEY,
+   apiKey: `SJUZCFQGMGQYW1YX3HYNQSA8B15HYUBI5G`,
  },
 };
