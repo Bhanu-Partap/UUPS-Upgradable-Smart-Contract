7114b6618823ce03d1c0908138af47aff18a8704
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-07-26 15:36:09 +0530

    UUPS

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,21 @@
-// require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-ethers");
 require("@openzeppelin/hardhat-upgrades");
-require("@nomiclabs/hardhat-etherscan");
+// require("@nomiclabs/hardhat-etherscan");
+require("@nomicfoundation/hardhat-toolbox");
 
-require("dotenv").config();
+
 
 module.exports = {
  solidity: "0.8.10",
  networks: {
-   kovan: {
-     url: `https://sepolia.infura.io/v3/ace13403824d4f809e011d9d547a4aa1`,
-     accounts: `56fe1fd10f0203c190640fb0bad78e9bf29d10fc2fbc97ee1ff49ee4acd2d08a`,
+   sepolia: {
+     url: "https://sepolia.infura.io/v3/ace13403824d4f809e011d9d547a4aa1",
+     accounts: [`56fe1fd10f0203c190640fb0bad78e9bf29d10fc2fbc97ee1ff49ee4acd2d08a`],
    },
  },
  etherscan: {
-   apiKey: `SJUZCFQGMGQYW1YX3HYNQSA8B15HYUBI5G`,
+   apiKey: {
+    sepolia:"ace13403824d4f809e011d9d547a4aa1"
+   },
  },
 };
--- scripts/deploy-1.js
+++ scripts/deploy-1.js
@@ -1,6 +1,6 @@
 const { ethers, upgrades } = require("hardhat");
 
-const SLICES = 8;
+const SLICES = 9;
 async function main() {
  const Pizza = await ethers.getContractFactory("Pizza");
 
@@ -9,9 +9,13 @@ async function main() {
  const pizza = await upgrades.deployProxy(Pizza, [SLICES], {
    initializer: "initialize",
  });
- await pizza.deployed();
-
- console.log("Pizza deployed to:", pizza.address);
+ await pizza.waitForDeployment();
+ const address = await ("Pizza deployed to:", pizza.getAddress())
+ console.log(address);
 }
+
+// contract address
+// x5ac6ad85b881ef3087d581EaE059De29A269d66a
+
 
 main();
