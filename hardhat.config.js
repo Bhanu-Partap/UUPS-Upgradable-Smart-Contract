b5f40e1a1eefd6b1d9705bbe445bb2feb5e60ff9
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-07-26 13:52:32 +0530

    UUPS

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
-require("@nomicfoundation/hardhat-toolbox");
+// require("@nomiclabs/hardhat-ethers");
+require("@openzeppelin/hardhat-upgrades");
+require("@nomiclabs/hardhat-etherscan");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
+
 module.exports = {
-  solidity: "0.8.10",
+ solidity: "0.8.10",
+ networks: {
+   kovan: {
+     url: `https://sepolia.infura.io/v3/ace13403824d4f809e011d9d547a4aa1${process.env.INFURA_API_KEY}`,
+     accounts: [process.env.PRIVATE_KEY],
+   },
+ },
+ etherscan: {
+   apiKey: process.env.ETHERSCAN_API_KEY,
+ },
 };
